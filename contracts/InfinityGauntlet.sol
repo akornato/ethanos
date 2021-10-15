8f434f1f8247c3a0ced91ed0702ea3b222d89987
akornato <artur.kornatowski@gmail.com>
2021-10-15 07:46:51 +0200

    remove strcmp

--- contracts/InfinityGauntlet.sol
+++ contracts/InfinityGauntlet.sol
@@ -44,20 +44,4 @@ contract InfinityGauntlet {
         }
         revert("Infinity stone not found");
     }
-
-    function memcmp(bytes memory a, bytes memory b)
-        internal
-        pure
-        returns (bool)
-    {
-        return (a.length == b.length) && (keccak256(a) == keccak256(b));
-    }
-
-    function strcmp(string memory a, string memory b)
-        internal
-        pure
-        returns (bool)
-    {
-        return memcmp(bytes(a), bytes(b));
-    }
 }
